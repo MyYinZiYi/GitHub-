73cecefef18f188228a621ddf2972d5eaf88245c
YZYmy <2832425415@qq.com>
2022-09-29 09:44:49 +0800

    style(custom): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 项目打开之后白屏问题
+  publicPath: './',
+  devServer: {
+    // 端口号
+    port: 2832,
+    // 主机名
+    host: 'localhost',
+    // 启动项目是否自动打开浏览器
+    open: true,
+    // 是否开启HTTPS
+    https: false,
+    // 配置跨域
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ["^" + process.env.VUE_APP_BASE_API]: ""
+        }
+      }
+    }
+
+  }
 })
+
+
